bd7882afe0b0c9fadf8c5d5c03b254cb3d807b9b
CyberViper <mohammadtanveer@gmail.com>
2025-02-04 20:23:46 +0530

    feat: add account related app

--- nexora_backend/nexora_backend/settings.py
+++ nexora_backend/nexora_backend/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+AUTH_USER_MODEL = 'account.User'
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=30), 
     'REFRESH_TOKEN_LIFETIME': timedelta(days=180), 
@@ -61,6 +63,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
     'rest_framework',  # Django REST framework
     'rest_framework_simplejwt',  # JWT support
     'corsheaders',  # CORS handling
